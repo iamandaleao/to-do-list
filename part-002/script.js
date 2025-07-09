ffaa06ce21a48de11321206c840dff0298b97a69
Amanda Leão <amandabibiano144@gmail.com>
2025-07-09 10:33:00 -0300

    feat: add second test

---
+++ part-002/script.js
@@ -0,0 +1,18 @@
+function cursor () {
+  const caderno = document.getElementById("caderno")
+  const novoitem = caderno.value
+  const iditem = crypto.randomUUID()
+
+  const botaoremover = `<button onclick="remover('${iditem}')"> Remover</button>`
+
+  const listadecompras = document.getElementById("listadecompras")
+   
+  listadecompras.innerHTML += `<li id="${iditem}">${novoitem} ${botaoremover}</li>`
+
+  caderno.value = ""
+}
+
+function remover (iditem) {
+  document.getElementById(iditem).remove()
+}
+ 
